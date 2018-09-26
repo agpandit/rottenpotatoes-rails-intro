9912d5740ea4e4e65380d0d6685261e925f99c32
EC2 Default User <ec2-user@ip-172-31-20-199.us-east-2.compute.internal>
2018-09-26 16:17:50 +0000

    Try 1 Part 1( with controller code)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,14 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    
+    if params[:sort_order].nil?
+      @movies = Movie.all
+    elsif params[:sort_order] == 'byTitle'
+      @movies = Movie.find(:all, :order => :title)
+    elsif params[:sort_order] == 'byReleaseDate'
+      @movies = Movie.find(:all, :order => :release_date)
+    end
   end
 
   def new
